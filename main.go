b3bbfbe259085c236507a88b7b30d1ed24ee6ca9
Rinards Sondors <rinards.sondors@gmail.com>
2023-11-20 21:33:02 +0000

    asdsad

--- main.go
+++ main.go
@@ -7,9 +7,17 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Static("/", "./index.html")
+	app.Static("/", "./public/")
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("index.html")
+	})
+
+	app.Get("/home", func(c *fiber.Ctx) error {
+		return c.SendFile("index.html")
+	})
+
+	app.Get("/about", func(c *fiber.Ctx) error {
 		return c.SendFile("index.html")
 	})
 
